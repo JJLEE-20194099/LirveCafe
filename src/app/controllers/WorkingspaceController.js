de07c027508f727f1516953b5bda31a31b85030b
JJLee-20194099 <JJLee-20194099>
2021-12-12 16:20:48 +0700

    Create workingspace controller

--- src/app/controllers/WorkingspaceController.js
+++ src/app/controllers/WorkingspaceController.js
@@ -1,28 +1,124 @@
-import Book from '../models/Book.js';
-import Order from '../models/Order.js';
-import Cart from '../models/Cart.js';
-import Orders from '../models/Orders.js';
+import Workingspace from '../models/Workingspace.js';
+import Coffee from '../models/Coffee.js';
+import Food from '../models/Food.js';
 import User from '../models/User.js';
-import Promo from '../models/Promo.js';
-
-import Rank from '../constants/user.rank.js';
-
-import Comment from '../models/Comment.js';
-import Reply from '../models/Reply.js';
-
-
-import {
+import { 
     singleMongooseDocumentToObject,
     mongooseDocumentsToObject
 } from '../../support_lib/mongoose.js';
 
 const WorkingspaceController = {
-    // GET /workingspace/index
+
+    // GET /workingspaces/list
+
     index(req, res, next) {
-        res.render('workingspace/index.hbs')
+        Workingspace.find({})
+            .then((workingspaces) => {
+                res.render('workingspaces/list/list.hbs', {
+                    workingspaces: mongooseDocumentsToObject(workingspaces),
+                    user: res.locals.user
+                })
+            }).catch(next);
+    },
+    
+    // GET /workingspaces/:slug
+    show(req, res, next) {
+        Workingspace.findOne({slug: req.params.slug})
+            .then((workingspace) => {
+                res.render('workingspaces/item/workingspace_info.hbs' , {
+                    workingspace: singleMongooseDocumentToObject(workingspace),
+                    user: res.locals.user
+                })
+            }).catch(next)
+    },
+
+    // GET /workingspaces/create
+    create(req, res, next) {
+
+        Promise.all([Coffee.find({}), Food.find({}), User.findOne({
+                _id: req.signedCookies.userId
+            })])
+            .then(([coffee, food, user]) => {
+               
+                coffee = mongooseDocumentsToObject(coffee)
+                food = mongooseDocumentsToObject(food)
+                var u = '';
+                if (user) {
+                    u = singleMongooseDocumentToObject(user)
+                } else {
+                    u = res.locals.user
+                }
+                console.log(food)
+                if (!u) {
+                    res.clearCookie("userId");
+                    res.render('users/workingspaces/item/create.hbs', {
+                        
+                        coffee: coffee,
+                        food: food
+                    });
+                } else {
+                    res.render('users/workingspaces/item/create.hbs', {
+                        user: u,
+                        
+                        coffee: coffee,
+                        food: food
+                    });
+                }
+
+            }).catch(next)
+    
+    },
+
+    //POST /workingspaces/save
+    save(req, res, next) {
+        req.body.image = "http://www.davidkrugler.com/s/River-Lights-8318.jpg";
+        const workingspace = new workingspace(req.body);
+        Workingspace.save()
+            .then(() => res.redirect('/users/stored/workingspaces'))
+            .catch(next)
+    },
+
+    // GET /workingspaces/:id/edit
+    edit(req, res, next) {
+        Workingspace.findOne({_id: req.params.id})
+            .then((workingspace) => {
+                res.render('workingspaces/item/edit.hbs', {
+                    workingspace: singleMongooseDocumentToObject(workingspace),
+                    user: res.locals.user
+                })
+            })
+            .catch(next)
+    },
+
+    // PUT /workingspaces/:id
+    update(req, res, next) {
+        Workingspace.updateOne({_id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next)
+    },
+
+    // SOFT DELETE /workingspaces/:id
+    softDelete(req, res, next) {
+        Workingspace.delete({_id: req.params.id})
+        .then(() => res. redirect('back'))
+        .catch(next);
+    },
+
+    // DEEP DELETE /books/:id/force
+    deepDelete(req, res, next) {
+        Workingspace.deleteOne({_id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next);
+    },
+
+    // RESTORE BOOK (PATCH) /books/:id/restore
+    restore(req, res, next) {
+        Workingspace.restore({_id: req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next);
     }
 
 
-};
+}; 
 
 export default WorkingspaceController;
