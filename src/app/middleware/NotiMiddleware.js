21dad2a2d8aafc1d28e5698b661f6006e7fb4e27
JJLee-20194099 <JJLee-20194099>
2021-12-13 17:57:44 +0700

    Add noti middleware, save notis in browser memory

---
+++ src/app/middleware/NotiMiddleware.js
@@ -0,0 +1,43 @@
+import User from '../models/User.js';
+import Noti from '../models/Noti.js';
+
+import {
+    singleMongooseDocumentToObject,
+    mongooseDocumentsToObject
+} from '../../support_lib/mongoose.js';
+
+const NotiMiddleware = {
+
+
+    getNotiInfo: function (req, res, next) {
+        if (req.signedCookies.userId) {
+            User.findOne({
+                    _id: req.signedCookies.userId
+                })
+                .then(user => {
+                    
+                    if (user) {
+                        console.log(res.locals.user.username)
+                        return Noti.find({
+                            receiver: res.locals.user.username
+                        }).sort({'createdAt': -1})
+                    } else {
+
+                        next();
+                    }
+
+                }).then((notis) => {
+                    if (notis) {
+                        res.locals.notis = mongooseDocumentsToObject(notis);
+                    }
+                    next();
+                })
+
+        } else {
+            next();
+        }
+
+    }
+}
+
+export default NotiMiddleware;
