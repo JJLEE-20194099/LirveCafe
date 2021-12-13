bc5ff4757feb320c0ad7fb85723db9881ef917ae
JJLee-20194099 <JJLee-20194099>
2021-12-13 17:57:00 +0700

    Add Noti model

---
+++ src/app/models/Noti.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import slug from 'mongoose-slug-generator';
+import mongooseDelete from 'mongoose-delete';
+
+const {Schema} = mongoose;
+
+mongoose.plugin(slug);
+const Noti = new Schema(
+    {
+        itemId: {type: String, required: true},
+        sender: {type: String, required: true},
+        receiver: {type: String, required: true},
+        where_url: {type: String, required: true},
+        avatar: {type: String, required: true},
+    },
+    {
+        timestamps: true
+    }
+);
+
+Noti.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: 'all',
+});
+
+export default mongoose.model('Noti', Noti)
+
+
