fb2f9b4722e5cccf1659e42edd3ca83ee0e54f29
JJLee-20194099 <JJLee-20194099>
2021-12-28 16:01:37 +0700

    Update noti route

--- src/routes/noti.js
+++ src/routes/noti.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import controllers from '../app/controllers/NotificationController.js';
 
 router.get('/:username', controllers.getNotisByUser)
+router.post('/index/:id', controllers.getNotiById)
 
 export default router;
